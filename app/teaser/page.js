847846df4fe215e1a95cfd5a2d7dd8321b72b31c
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-05-18 12:10:08 +0200

    merged teaser to main

--- app/teaser/page.js
+++ app/teaser/page.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ProductTeaser from '../components/ProductTeaser/ProductTeaser';
 
 //styles
-import styles from './teaser.module.scss'
+import styles from './teaser.module.scss';
 
 const page = () => {
-    return <div className={styles.teaser}>
-        <h2>Teaser component</h2>
-        <ProductTeaser />
-  </div>;
+  return (
+    <div className={styles.teaser}>
+      <h2>Teaser component</h2>
+      <ProductTeaser />
+    </div>
+  );
 };
 
 export default page;
